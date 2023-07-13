41328f28438f04006e86031ab452ac310ebf1666
Reuben Chege <reubenchege7@gmail.com>
2023-07-13 13:28:05 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,5 +5,7 @@
  * Return: Always 0 (success)
  */
 int main(void)
-	puts("Programming is like building a multilingual puzzle\n");
+{     
+	puts ("Programming is like building a multilingual puzzle\n");
 	return (0);
+}
